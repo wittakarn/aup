d9286a1e4344195c152f408437e1ece7c810f70b
wittakarn <wittakarn.expert@gmail.com>
2019-08-26 21:54:04 +0700

    Increase breaking time

--- src/main/java/com/thaisoftplus/aup/thread/Main.java
+++ src/main/java/com/thaisoftplus/aup/thread/Main.java
@@ -140,6 +140,6 @@ public class Main implements Runnable {
 
     private static boolean isBreakTime() {
         int hour = DateHelper.getCurrentHour("Asia/Bangkok");
-        return hour > 13 && hour < 16;
+        return hour > 13 && hour < 19;
     }
 }
